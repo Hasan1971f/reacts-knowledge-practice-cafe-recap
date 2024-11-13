0e068a5ba1d96069d3145792d68bc3102a94c6f0
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-13 18:26:43 +0600

    added a bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 
+import { useState } from 'react'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
- 
+ const [bookmarks, setBookmarks]= useState([])
+
+ const handleAddToBookmark = blog =>{
+  const newBookmarks = [...bookmarks, blog]
+  setBookmarks(newBookmarks)
+ }
 
   return (
     <>
       
-      <h1 className='text-6xl bg-red-600'>Knowledge Cafee Recap</h1>
+      <Header></Header>
+     <div className='md:flex max-w-7xl mx-auto'>
+     <Blogs  handleAddToBookmark={handleAddToBookmark}></Blogs>
+     <Bookmarks bookmarks={bookmarks}></Bookmarks>
+     </div>
       
      
     </>
   )
 }
+
+
+
 
 export default App
